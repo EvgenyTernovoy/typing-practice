714a4acf3000adf2223fabe8189dc4bed5b664ff
Eugeny <ternovoy-ev@ya.ru>
2021-03-06 15:29:41 +0300

    ADD email validation

--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -7,17 +7,27 @@ export default class LoginService {
 
   // Try to define better types
   public async login(email: Credentials['email'], password: Credentials['password']): Promise<User> {
+    if (!this.isValidEmail(email)) {
+      throw new Error("Email is invalid");
+    }
+
     const registeredUsers = await this.getRegisteredUsers()
 
     const user = registeredUsers.default.filter((u: User) => u.email === email && u.password === password)[0]
 
     if(!user) {
-      throw new Error("User not find");
+      throw new Error("User is not found");
     }
 
     const User = this.userService.getConstructorByRole(user.role)
 
     return User.from(user)
+  }
+
+  isValidEmail(email: Credentials['email']): email is Credentials['email'] {
+    const regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    return regexp.test(email)
   }
 
   private getRegisteredUsers(): Promise<any> {
