e3293ca51a6a3372935193b4dc82338ee37a3a38
Eugeny <ternovoy-ev@ya.ru>
2021-03-10 16:40:18 +0300

    FIX use operation

--- src/hooks/use-operations.ts
+++ src/hooks/use-operations.ts
@@ -1,12 +1,15 @@
 import Services from "../services";
-import { useContext } from "react";
-import type { User } from "../entities/user";
+import {useContext} from "react";
+import type {User} from "../entities/user";
 import or from "../utils/or";
 import {Admin} from "../entities/admin";
 import {Moderator} from "../entities/moderator";
 
 export default function useOperations(user: User, currentUser: User) {
-  const { userService } = useContext(Services);
+  const {userService} = useContext(Services);
+  if (!currentUser) {
+    return []
+  }
   const adminOrModerator = or(Admin, Moderator);
 
   return userService.getAvailableOperations(user, adminOrModerator(currentUser));
